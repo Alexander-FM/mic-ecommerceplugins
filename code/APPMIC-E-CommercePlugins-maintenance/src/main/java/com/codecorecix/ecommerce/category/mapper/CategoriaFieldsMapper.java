a5ce5bfb823801fedf3cb078e4c32da25a87dcf7
Luigui Alexander Fuentes Medina <luiguiafm@ext.zara.com>
2024-07-14 18:17:46 -0500

    Master: Added more configurations

---
+++ code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/category/mapper/CategoriaFieldsMapper.java
@@ -0,0 +1,20 @@
+package com.codecorecix.ecommerce.category.mapper;
+
+import java.util.List;
+
+import com.codecorecix.ecommerce.category.api.dto.request.CategoryRequestDto;
+import com.codecorecix.ecommerce.category.api.dto.response.CategoryResponseDto;
+import com.codecorecix.ecommerce.event.entities.Category;
+import com.codecorecix.ecommerce.event.mapper.GenericFieldsMapper;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CategoriaFieldsMapper extends GenericFieldsMapper<CategoryResponseDto, Category> {
+
+  Category sourceToDestination(final CategoryRequestDto source);
+
+  CategoryResponseDto destinationToSource(final Category destination);
+
+  @Override
+  List<CategoryResponseDto> toDto(final List<Category> entityList);
+}
---
+++ code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/category/service/CategoriaServiceImpl.java
@@ -0,0 +1,27 @@
+package com.codecorecix.ecommerce.category.service;
+
+import java.util.List;
+
+import com.codecorecix.ecommerce.category.api.dto.request.CategoryRequestDto;
+import com.codecorecix.ecommerce.category.api.dto.response.CategoryResponseDto;
+import com.codecorecix.ecommerce.category.mapper.CategoriaFieldsMapper;
+import com.codecorecix.ecommerce.category.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+  private final CategoryRepository repository;
+
+  private final CategoriaFieldsMapper mapper;
+
+  public CategoriaServiceImpl(CategoryRepository repository, CategoriaFieldsMapper mapper) {
+    this.repository = repository;
+    this.mapper = mapper;
+  }
+
+  @Override
+  public List<CategoryResponseDto> categoryList(CategoryRequestDto categoryRequestDto) {
+    return this.mapper.toDto(this.repository.findAll());
+  }
+}
