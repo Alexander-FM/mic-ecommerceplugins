a05776cbe647be0cb752340a657a60d915aa10be
Alexander Fuentes Medina <81717888+Alexander-FM@users.noreply.github.com>
2024-08-27 12:04:30 -0500

    Construir el CRUD de Marcas (#6)
    * [feature/TEC-INTEGRA-005] feat: Se está terminando de construir el servicio.
    
    * [feature/TEC-INTEGRA-005] feat: Se creo el rest controller para marcas, ya está disponible para su uso.
    
    ---------
    
    Co-authored-by: Pablo Andre Giraldo Moran <pgiraldo.ext@acciona.com>

---
+++ code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/maintenance/brand/repository/BrandRepository.java
@@ -0,0 +1,20 @@
+package com.codecorecix.ecommerce.maintenance.brand.repository;
+
+import java.util.List;
+
+import com.codecorecix.ecommerce.event.entities.Brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+
+  List<Brand> findByIsActiveIsTrue();
+
+  @Modifying
+  @Query("UPDATE Brand B SET B.isActive = ?1 WHERE B.id = ?2")
+  void desactivateOrActivateBrand(final Boolean isActive, final Integer id);
+}
---
+++ code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/maintenance/brand/utils/BrandConstants.java
@@ -0,0 +1,12 @@
+package com.codecorecix.ecommerce.maintenance.brand.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BrandConstants {
+
+  public static final String NO_EXIST = "The brand no exist in BD";
+
+  public static final String UNPROCESSABLE_ENTITY_EXCEPTION = "The request is incorrectly formatted";
+}
