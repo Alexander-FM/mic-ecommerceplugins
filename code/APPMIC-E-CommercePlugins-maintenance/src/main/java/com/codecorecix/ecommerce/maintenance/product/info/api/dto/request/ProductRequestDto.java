05155d56dbd7c3c488d694e526bfdfadbaf9b334
Alexander Fuentes Medina <81717888+Alexander-FM@users.noreply.github.com>
2024-08-27 15:43:07 -0500

    Corrección del mapeo de productos y agregar validaciones en la request con JAKARTA. (#8)
    * [bugfix/TEC-INTEGRA-200] feat: Complete modification of products.
    
    * [bugfix/TEC-INTEGRA-200] feat: Complete modification of products.
    
    * [bugfix/TEC-INTEGRA-200] feat: Modification in insertDataProducts.json.
    
    ---------
    
    Co-authored-by: Pablo Andre Giraldo Moran <pgiraldo.ext@acciona.com>

--- code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/maintenance/product/info/api/dto/request/ProductRequestDto.java
+++ code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/maintenance/product/info/api/dto/request/ProductRequestDto.java
@@ -9,6 +9,9 @@ import com.codecorecix.ecommerce.event.entities.Category;
 import com.codecorecix.ecommerce.event.entities.ProductDetail;
 import com.codecorecix.ecommerce.event.entities.ProductImage;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,22 +25,35 @@ public class ProductRequestDto implements Serializable {
 
   private Integer id;
 
+  @NotNull(message = "The barcode is null, please fill.")
+  @NotEmpty(message = "The barcode is empty, please fill.")
+  @Size(min = 1, max = 20, message = "The size must be between 2 and 50 characters.")
   private String barCode;
 
+  @NotNull(message = "The name of product is null, please fill.")
+  @NotEmpty(message = "The name of product is empty, please fill.")
   private String name;
 
+  @NotNull(message = "The description of product is null, please fill.")
+  @NotEmpty(message = "The description of product is empty, please fill.")
   private String description;
 
+  @NotNull(message = "The price of product is null, please fill.")
   private Double price;
 
+  @NotNull(message = "The stock of product is null, please fill.")
   private Integer stock;
 
+  @NotNull(message = "The product must be associated with a category, please fill.")
   private Category category;
 
+  @NotNull(message = "The product must be associated with a brand, please fill.")
   private Brand brand;
 
+  @NotNull(message = "The field is active is null, please fill.")
   private Boolean isActive;
 
+  @NotNull(message = "The field is recommended is null, please fill.")
   private Boolean isRecommended;
 
   private List<ProductImage> images = new ArrayList<>();
