561698a901f44391f35794dde49569a174a0db13
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2026-02-16 14:17:27 -0500

    feat: Implement UserConfig for password encoding and remove obsolete security filters and properties

---
+++ code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/maintenance/user/config/UserConfig.java
@@ -0,0 +1,14 @@
+package com.codecorecix.ecommerce.maintenance.user.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class UserConfig {
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
