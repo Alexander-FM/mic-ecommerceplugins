c05a080d9cabf12fad9021942903726f732417df
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2026-07-19 22:46:42 -0500

    feat: initialize common module, configure WebClient, and add Kubernetes orchestration resources for infrastructure testing.

--- code/APPMIC-E-CommercePlugins-common/src/main/java/com/codecorecix/ecommerce/config/WebClientConfig.java
+++ code/APPMIC-E-CommercePlugins-common/src/main/java/com/codecorecix/ecommerce/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.codecorecix.ecommerce.utils.httpclient;
+package com.codecorecix.ecommerce.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +16,8 @@ public class WebClientConfig {
 
   @Bean
   WebClient simpleWebClient() {
-    return WebClient.builder().build();
+    return WebClient
+        .builder()
+        .build();
   }
 }
--- code/src/test/resources/k8s/tear-down/factory-reset.sh
+++ code/src/test/resources/k8s/tear-down/factory-reset.sh
@@ -5,24 +5,24 @@ echo "🛑 Deteniendo todos los microservicios y la infraestructura en el namesp
 
 echo "--- Eliminando Microservicios ---"
 
-kubectl delete -f auth/ds-appmic-e-commerceplugins-auth.yaml -n $NAMESPACE
-kubectl delete -f maintenance/ds-appmic-e-commerceplugins-maintenance.yaml -n $NAMESPACE
-kubectl delete -f orders/ds-appmic-e-commerceplugins-orders.yaml -n $NAMESPACE
-kubectl delete -f gateway/ds-appmic-e-commerceplugins-gateway.yaml -n $NAMESPACE
+kubectl delete -f ../auth/ds-appmic-e-commerceplugins-auth.yaml -n $NAMESPACE
+kubectl delete -f ../maintenance/ds-appmic-e-commerceplugins-maintenance.yaml -n $NAMESPACE
+kubectl delete -f ../orders/ds-appmic-e-commerceplugins-orders.yaml -n $NAMESPACE
+kubectl delete -f ../gateway/ds-appmic-e-commerceplugins-gateway.yaml -n $NAMESPACE
 
 echo "--- Eliminando Base de datos ---"
-kubectl delete -f bd/ds-mysql.yaml -n $NAMESPACE
+kubectl delete -f ../bd/ds-mysql.yaml -n $NAMESPACE
 
 echo "--- Eliminando Secretos y Configmap y Almacenamiento"
-kubectl delete -f secret/secret.yaml -n $NAMESPACE
-kubectl delete -f configmap/configmap.yaml -n $NAMESPACE
-kubectl delete -f configmap/configmap-mysql.yaml -n $NAMESPACE
-kubectl delete -f bd/mysql-pvc.yaml -n $NAMESPACE
+kubectl delete -f ../secret/secret.yaml -n $NAMESPACE
+kubectl delete -f ../configmap/configmap.yaml -n $NAMESPACE
+kubectl delete -f ../configmap/configmap-mysql.yaml -n $NAMESPACE
+kubectl delete -f ../bd/mysql-pvc.yaml -n $NAMESPACE
 # Los PVs no tienen namespace, así que se eliminan sin -n
-kubectl delete -f bd/mysql-pv.yaml
+kubectl delete -f ../bd/mysql-pv.yaml
 
 echo "--- Eliminando el Cluster role binding RBAC ---"
-kubectl delete -f setup/rbac.yaml -n $NAMESPACE
+kubectl delete -f ../setup/rbac.yaml
 
 echo "✅ Limpieza completada en el namespace '$NAMESPACE'."
 echo "Para eliminar el namespace por completo, ejecuta: kubectl delete namespace $NAMESPACE"
