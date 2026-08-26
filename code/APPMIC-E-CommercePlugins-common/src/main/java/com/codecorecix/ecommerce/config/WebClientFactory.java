71ee794d4f64c9ddbe4de5dfc61e4e7a9cf6ad19
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2026-08-26 15:47:39 -0500

    feat: add Kubernetes configuration support to WebClientFactory for environment detection

--- code/APPMIC-E-CommercePlugins-common/src/main/java/com/codecorecix/ecommerce/config/WebClientFactory.java
+++ code/APPMIC-E-CommercePlugins-common/src/main/java/com/codecorecix/ecommerce/config/WebClientFactory.java
@@ -2,6 +2,7 @@ package com.codecorecix.ecommerce.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,12 +21,16 @@ public class WebClientFactory {
 
   private final WebClient.Builder internalApiBuilderLoadBalanced;
 
+  // Inyectamos la variable. Si no existe (ej. corriendo en local nativo), asume true por defecto
+  @Value("${spring.cloud.kubernetes.enabled:true}")
+  private boolean isK8sEnabled;
+
   /**
    * Devuelve el WebClient correcto basándose en sí la URL apunta a un entorno local o de cluster. <p> @param serviceUrl La URL final ya
    * resuelta (ej.: "<a href="http://localhost:8082"> Url local </a>" o "<a href="http://appmic-employees"> Url kubernetes </a>)</p>
    */
   public WebClient retrieveWebClient(final String serviceUrl) {
-    if (isLocalUrl(serviceUrl)) {
+    if (!isK8sEnabled || isLocalUrl(serviceUrl)) {
       // --- ESCENARIO LOCAL ---
       log.info("Web Client Factory: Detectado Localhost. Usando cliente SIMPLE: {}", serviceUrl);
       return simpleWebClient
@@ -43,10 +48,10 @@ public class WebClientFactory {
 
   /**
    * Devuelve un WebClient INTERNO y SEGURO que cambia entre local y K8s.
-   * Automáticamente añade el token de Client Credentials.
+   * Automáticamente, añade el token de Client Credentials.
    */
   public WebClient retrieveInternalWebClient(final String serviceUrl) {
-    if (isLocalUrl(serviceUrl)) {
+    if (!isK8sEnabled || isLocalUrl(serviceUrl)) {
       // Para local, usamos un cliente simple pero con el filtro OAuth2.
       // Esto permite probar el flujo de token incluso en local.
       log.info("Web Client Factory: Usando cliente INTERNO (simple + oauth2) para URL local: {}", serviceUrl);
